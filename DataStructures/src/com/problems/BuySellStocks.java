fb7baf35881ea77ee015653ae8dab8950bfc31e9
Ravi Thakur <ravithakur2k@gmail.com>
2022-10-04 08:13:11 -0400

    Buy and Sell Stocks, Valid Palindrome

---
+++ DataStructures/src/com/problems/BuySellStocks.java
@@ -0,0 +1,40 @@
+package com.problems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class BuySellStocks {
+
+    public static void main(String args[]) {
+        System.out.println(optimizedProfitSoln(new int[]{7,2,5,3,1,4}));
+    }
+
+    public static int profit(int[] prices) {
+        int max = 0;
+        for (int p1 = 0; p1 < prices.length - 1; p1++) {
+            int p2 = p1 + 1;
+            while (p2 < prices.length) {
+                int diff = prices[p2] - prices[p1];
+                if (diff > max) max = diff;
+                p2++;
+            }
+        }
+        return max;
+    }
+
+    public static int optimizedProfitSoln(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min){
+                min = prices[i];
+            }
+
+            if (prices[i] - min > profit) {
+                profit = prices[i] - min;
+            }
+        }
+        return profit;
+
+    }
+}
---
+++ DataStructures/src/com/problems/ValidPalindrome.java
@@ -0,0 +1,29 @@
+package com.problems;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidPalindrome {
+
+    public static void main(String args[]) {
+        isValidPalindrome("A man, a plan, a canal: Panama");
+
+    }
+
+    public static boolean isValidPalindrome(String str) {
+        str = str.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str.length(); i++){
+            if(Character.isDigit(str.charAt(i)) || Character.isLetter(str.charAt(i))) {
+                sb.append(str.charAt(i));
+            }
+        }
+
+        int p2 = sb.length() - 1;
+        for(int i = 0; i < sb.length()/2; i++){
+            if(sb.charAt(i) != sb.charAt(p2-i)) return false;
+        }
+        return true;
+    }
+
+}
